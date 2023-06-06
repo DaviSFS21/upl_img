9ab4abe01c7e65084feca88460b8721a96f15600
Davi Soares <davisfs2110@gmail.com>
2023-06-05 21:53:56 -0300

    Otimização do código

--- index.php
+++ index.php
@@ -4,30 +4,40 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="opera">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Upload de imagem com MySQL</title>
+    <title>Upload de imagem com PHP</title>
 </head>
 <body>
     <!-- Formulário para envio das imagens -->
-    <form enctype="multipart/form-data" action="" method="POST">
-        <input type="file" name="img_prod"><br><br>
-        <button type="submit">Enviar</button><br><br>
+    <form enctype="multipart/form-data" action="" method="POST"><br><br>
+        <input type="file" name="img_prod">
+        <button type="submit">Enviar nova imagem</button><br><br>
     </form>
     <?php
+
+    $arquivos = glob('assets/img_prod/*');
+
+    if ($arquivos !== false) {
+        // Percorre cada arquivo e o exclui
+        foreach ($arquivos as $arquivo) {
+            if (is_file($arquivo)) {
+                unlink($arquivo);
+            }
+        }
+    }
         /* Condição para só executar o código quando o formulário for enviado */
         if(isset($_FILES['img_prod'])){
 
             /* Declaração da variável da imagem e apresentação dos detalhes do arquivo */
             $img_prod = $_FILES['img_prod'];
             echo "Arquivo enviado!";
-            echo "<br><br>" . $img_prod['name'];
-            echo "<br><br>" . $img_prod['size'] . " Bytes";
-            echo "<br><br>" . $img_prod['tmp_name'];
+            echo "<br>Nome do arquivo: " . $img_prod['name'];
+            echo "<br>Tamanho do arquivo: " . $img_prod['size'] . " Bytes";
 
             /* Declaração do novo caminho da imagem e criação do uniqid() para mudar o local da imagem, do local temporário ao source do servidor */
             $pasta = "assets/img_prod/";
             $novoNomeImg = uniqid();
             $extensaoImg = strtolower(pathinfo($img_prod['name'], PATHINFO_EXTENSION));
-            echo "<br><br>" . $extensaoImg;
+            echo "<br>Formato da imagem: " . $extensaoImg;
 
             /* Condições caso o upload sofra um erro, caso a extensão seja a errada, ou, caso a imagem seja muito pesada */
             if($img_prod['error']){
@@ -63,12 +73,12 @@
 
             $novoPath = $pasta . $novoNomeImg . "." . $extensaoImg;
 
-            echo "<br><br>" . $novoPath;
-
-            /* Movendo a imagem e apresentando-a na tela */
+            echo "<br>Path do caminho do arquivo temporário no servidor: " . $img_prod['tmp_name'];
+            echo "<br>Caminho definitivo no servidor: " . $novoPath;
+    
             move_uploaded_file($img_prod['tmp_name'], $novoPath);
 
-            echo "<br><br><img src='$novoPath' width='80%' height='80%'>";
+            echo "<br><br><img src='$novoPath' width='200em' height='30%'>";
         }
     ?>
 </body>
